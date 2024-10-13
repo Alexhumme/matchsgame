e8c200e1e827fa0c7bab4a4ed09c4fc357c66304
Alexhumme <valdelex3@gmail.com>
2024-10-13 16:05:51 -0500

    first commit

---
+++ src/main/java/uniguajira/matchsgame/Matchsgame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uniguajira.matchsgame;
+
+/**
+ *
+ * @author AlexVB
+ */
+public class Matchsgame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        game_scene gs = new game_scene();
+        
+    }
+}
