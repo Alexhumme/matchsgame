7acc85fcb0cac3aa9dff779012e0a626a5512d60
Alexhumme <valdelex3@gmail.com>
2024-10-16 11:20:01 -0500

    test demostrativo de git

--- src/main/java/uniguajira/matchsgame/Matchsgame.java
+++ src/main/java/uniguajira/matchsgame/Matchsgame.java
@@ -12,7 +12,5 @@ public class Matchsgame {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        game_scene gs = new game_scene();
-        
     }
 }
